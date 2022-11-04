a19d4637300551fb23b8cc90771fde74a550b653
RamSailopal <thelinuxguys@yahoo.com>
2022-11-04 09:34:23 +0000

    Adding an extended man pages option

---
+++ mane.ps1
@@ -0,0 +1,41 @@
+#
+#   Extended Man/Get-Help
+#
+#   Combines Get-Help output with Get-Member (to display command object details)
+#
+#   Use chk function to check that the command is valid
+#
+#
+Function chk($cmd) {
+    $fnd = 0
+    Get-Command | 
+    ForEach-Object { 
+        If ($_.name -eq $cmd ) { 
+            $fnd=1 
+        } 
+    }
+    return $fnd
+}
+if ( $args.length -eq 0) {
+    $rcmd = Read-Host "Which command do you want details for"
+    $valid=chk($rcmd)
+    if ($valid -eq 1) {
+        Get-Help $rcmd
+        $cmd = "$($rcmd) | Get-Member"
+        Invoke-Expression $cmd
+    }
+    else {
+        Write-Host -ForegroundColor Red "Command is not valid"
+    }
+}
+else {
+    $valid=chk($args[0])
+    if ($valid -eq 1) {
+        Get-Help $args[0]
+        $cmd = "$($args[0]) | Get-Member"
+        Invoke-Expression $cmd
+    }
+    else {
+        Write-Host -ForegroundColor Red "Command is not valid"
+    }
+}
